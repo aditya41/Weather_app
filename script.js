8289b7c891984f059f2f1ef3fac1fa2dc4be9185
aditya41 <aditya41@users.noreply.github.com>
2020-10-25 16:28:39 +0530

    style

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ $(() => {
     $('#btn').click(() => {
         let city = $('#inp').val()
         let temperature;
+        const unit = "metric";
         console.log(city)
-        let res = $.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6b0511b434c662647a69686ef23c49fc`, (data) => {
+        let res = $.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6b0511b434c662647a69686ef23c49fc&units=${unit}`, (data) => {
             temperature = data.main.temp
             console.log(temperature)
-            $('body').append(
+            $('.box_1').append(
                 $(`<div>`).text(temperature)
 
             )
